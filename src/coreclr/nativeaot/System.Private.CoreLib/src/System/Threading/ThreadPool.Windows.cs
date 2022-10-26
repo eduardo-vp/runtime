e0b53fd8e85ebb7f74cc93cda7ff33b097faf1eb
Eduardo Manuel Velarde Polar <evelardepola@microsoft.com>
2022-10-26 14:01:24 -0700

    Make state nullable in ThreadPool.Windows.cs

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Threading/ThreadPool.Windows.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Threading/ThreadPool.Windows.cs
@@ -380,7 +380,7 @@ namespace System.Threading
         private static RegisteredWaitHandle RegisterWaitForSingleObject(
              WaitHandle waitObject,
              WaitOrTimerCallback callBack,
-             object state,
+             object? state,
              uint millisecondsTimeOutInterval,
              bool executeOnlyOnce,
              bool flowExecutionContext)
