4a6263da69f52db923c814f958931c8440b1a27d
Eduardo Velarde <evelardepola@microsoft.com>
2026-04-20 18:17:03 -0700

    Update test

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/ArgumentValidationTests.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/ArgumentValidationTests.cs
@@ -402,25 +402,15 @@ namespace System.Net.Sockets.Tests
         [Fact]
         public void SelectPoll_InfiniteTimeSpan_Ok()
         {
-            using (Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
             {
-                host.Bind(new IPEndPoint(IPAddress.Loopback, 0));
-                host.Listen(1);
-                Task accept = host.AcceptAsync();
+                var list = new List<Socket> { s };
 
-                using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-                {
-                    s.Connect(new IPEndPoint(IPAddress.Loopback, ((IPEndPoint)host.LocalEndPoint).Port));
-
-                    var list = new List<Socket>();
-                    list.Add(s);
-
-                    // should be writable
-                    Socket.Select(null, list, null, Timeout.InfiniteTimeSpan);
-                    Socket.Select(null, list, null, -1);
-                    s.Poll(Timeout.InfiniteTimeSpan, SelectMode.SelectWrite);
-                    s.Poll(-1, SelectMode.SelectWrite);
-                }
+                // should be writable
+                Socket.Select(null, list, null, Timeout.InfiniteTimeSpan);
+                Socket.Select(null, list, null, -1);
+                s.Poll(Timeout.InfiniteTimeSpan, SelectMode.SelectWrite);
+                s.Poll(-1, SelectMode.SelectWrite);
             }
         }
 
